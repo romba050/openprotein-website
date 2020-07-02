80f6e8d8e5e94a443774d9e049b3aa32b9e9151d
bzyl <basilerommes@hotmail.com>
2020-07-02 14:14:51 +0200

    everything is a section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,11 +52,18 @@ const IndexPage: React.FC<PageProps> = ({ data, path }) => (
 
     <section className="section">
         <div className="container">
-            <h1 className="title">Section</h1>
+            <h1 className="title">Next Generation Protein Simulation</h1>
             <h2 className="subtitle">
-            A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+            <strong> OpenProtein </strong> is a new machine learning framework for <strong>modeling tertiary protein structure</strong>. Built on top of PyTorch, NGL Viewer and ProteinNet, it offers <strong>automatic differentiation, visualisation, built-in data sets</strong> and much more.
             </h2>
         </div>
+            
+        {/* <div className="card-footer-item"> */}
+            <a href="https://github.com/biolib/openprotein" className="button is-primary">
+                <p> OpenProtein on Github </p>
+            </a>
+        {/* </div> */}
+
     </section>
 
     <section className="section">
@@ -69,47 +76,70 @@ const IndexPage: React.FC<PageProps> = ({ data, path }) => (
         </div> */}
 
         {/* <figure className="image is-2721×1840"> */}
-        <figure className="image is-2721×1840">
-            <img src="src/images/examplemodelrun.png"></img>
-        </figure>
-    
-        <h1 className="title"> Next Generation Protein Simulation </h1>
-        <p> OpenProtein is a new machine learning framework for modeling tertiary protein structure. Built on top of PyTorch, NGL Viewer and ProteinNet, it offers automatic differentiation, visualisation, built-in data sets and much more. </p>
-        <div className="card-footer-item">
-            <a href="https://github.com/biolib/openprotein" className="button is-primary">
-                <p> OpenProtein on Github </p>
-            </a>
-        </div>
+        <section className="section">
+            <figure className="image is-2721×1840">
+                <img src="src/images/examplemodelrun.png"></img>
+            </figure>
+        </section>
 
-        {/* <Link to="https://github.com/biolib/openprotein">OpenProtein on Github</Link> */}
-        <p></p>
-        <hr/>
-        <h2>Meet OpenProtein</h2>
-        <p>We introduce a new machine learning framework, OpenProtein, for prediction of tertiary protein structure. The framework is based upon PyTorch and utilizes NGL Viewer for live visualization of the protein as it folds. OpenProtein parses the ProteinNet data set (derived from previous CASP competitions) directly using a memory efficient data loader and supports a wide range of features including built-in embedding, automatic differentiation and performance plotting.</p>
-        <h3>Fast Iteration</h3>
-        <p>We know how important quick iteration speed is in research. OpenProtein gives you feedback on your model changes in seconds.</p>
-        <h3>GPU Compatible</h3>
-        <p>All OpenProtein models are GPU compatible by default and the framework's custom PyTorch data loaders are optimized for memory efficiency.</p>
-        <p></p>
-        <h3>Automatic Training</h3>
-        <p>Simply focus on designing your model (in many cases less than 50 lines of code!) and the framework will handle the rest for you, including training and validation.</p>
-        <p></p>
-        <h2>Stay in the know</h2>
-        <p>Join the mailing list and get notified when a new version of OpenProtein is released</p>
+        <section className="section">
+            <div className="container">
+                <h1 className="title">Meet OpenProtein</h1>
+                <h2 className="subtitle">
+                    We introduce a new machine learning framework, OpenProtein, for prediction of tertiary protein structure. The framework is based upon PyTorch and utilizes NGL Viewer for live visualization of the protein as it folds. OpenProtein parses the ProteinNet data set (derived from previous CASP competitions) directly using a memory efficient data loader and supports a wide range of features including built-in embedding, automatic differentiation and performance plotting.
+                </h2>
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <h1 className="title">Fast Iteration</h1>
+                    We know how important quick iteration speed is in research. OpenProtein gives you feedback on your model changes in seconds.
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <h1 className="title">GPU Compatible</h1>
+                    All OpenProtein models are GPU compatible by default and the framework's custom PyTorch data loaders are optimized for memory efficiency.
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <h1 className="title">Automatic Training</h1>
+                Simply focus on designing your model (in many cases less than 50 lines of code!) and the framework will handle the rest for you, including training and validation.
+            </div>
+        </section>
+
+        <section className="section">
+            <div className="container">
+                <h1 className="title">Stay in the know</h1>
+                Join the mailing list and get notified when a new version of OpenProtein is released
+            </div>
+        </section>
+
         {/* <Link to="/early_access/"><button> Get early access </button></Link> */}
+        <section className="section">
         <div className="card-footer-item">
             <a href="/early_access/" className="button is-primary">
                 <p> Get early access </p>
             </a>
         </div>
-        <p></p>
-        <h3>Frequently Asked Questions</h3>
-        <h4>Is this yet another structure prediction tool?</h4>
-        <p>Quite the opposite in fact - OpenProtein is a framework for creating, training and evaluating models for protein structure prediction. It can be used to improve existing models too!</p>
-        <h4>What dependencies do I need to run OpenProtein?</h4>
-        <p>Open Protein runs in any Python 3 environment with PyTorch installed. A GPU is recommended, but not required.</p>
-        <h4>How do I contribute to OpenProtein?</h4>
-        <p>Simply create a pull request in the project's Github repository at <Link to="https://github.com/OpenProtein/openprotein">https://github.com/OpenProtein/openprotein</Link></p>
+        </section>
+
+        <section className="section">
+        <div className="container">
+            <h3 className="title">Frequently Asked Questions</h3>
+            <h4>Is this yet another structure prediction tool?</h4>
+            <p>Quite the opposite in fact - OpenProtein is a framework for creating, training and evaluating models for protein structure prediction. It can be used to improve existing models too!</p>
+            <h4>What dependencies do I need to run OpenProtein?</h4>
+            <p>Open Protein runs in any Python 3 environment with PyTorch installed. A GPU is recommended, but not required.</p>
+            <h4>How do I contribute to OpenProtein?</h4>
+            <p>Simply create a pull request in the project's Github repository at <Link to="https://github.com/OpenProtein/openprotein">https://github.com/OpenProtein/openprotein</Link></p>
+        </div>
+        </section>
+
     </Row>
     </section>
 
