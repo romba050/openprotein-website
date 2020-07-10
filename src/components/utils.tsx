98a899b6a2009028c5e2f844895208bfc823a8ef
bzyl <basilerommes@hotmail.com>
2020-07-10 14:50:48 +0200

    working version

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-class Utils extends React.Component {
-  // make Row alias for columns
-  Row: React.FC = (props) => (
-    <div className="columns is-mobile">{props.children}</div>
-  );
-  // make Col alias for column
-  Col: React.FC = (props) => <div className="column">{props.children}</div>;
-}
+// class Utils extends React.Component {
+//   // make Row alias for columns
+//   // synonymous with React.FunctionComponent
+//   Row: React.FC = (props) => (
+//     <div className="columns is-mobile">{props.children}</div>
+//   );
+//   // make Col alias for column
+//   Col: React.FC = (props) => <div className="column">{props.children}</div>;
+// }
 
 // had to rewrite as class methods not available for export
 // make Row alias for columns
@@ -19,7 +20,8 @@ export function Col(props) {
   return <div className="column">{props.children}</div>;
 }
 
-export default Utils;
+
+// // export default Utils;
 // export function Row;
 // export function Col;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 // import Image from "../components/image";
 import SEO from "../components/seo";
 import Img from "gatsby-image";
-import { Row, Col } from "../components/utils";
+import {Row, Col} from "../components/utils";
 
 // const IndexPage: React.FC<PageProps> = ({ data, path }) => (
 class IndexPage extends React.Component {
@@ -171,6 +171,41 @@ class IndexPage extends React.Component {
               </div>
             </section>
           </Row>
+        </section>
+
+        <section className="accordions">
+          <article className="accordion is-active">
+            <div className="accordion-header toggle">
+              <p>Hello World</p>
+            </div>
+            <div className="accordion-body">
+              <div className="accordion-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </div>
+          </article>
+          <article className="accordion">
+            <div className="accordion-header">
+              <p>Hello World</p>
+              <button className="toggle" aria-label="toggle"></button>
+            </div>
+            <div className="accordion-body">
+              <div className="accordion-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </div>
+          </article>
+          <article className="accordion">
+            <div className="accordion-header">
+              <p>Hello World</p>
+              <button className="toggle" aria-label="toggle"></button>
+            </div>
+            <div className="accordion-body">
+              <div className="accordion-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </div>
+          </article>
         </section>
 
         {/* </Layout> */}
